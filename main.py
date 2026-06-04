554510952a78e38fe9f7b906c76e0b96af8209d5
Gleb <glebsmailbox@gmail.com>
2026-06-04 21:22:02 +0200

    Added an endpoint with image generation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from routers import ai_agent, calculator
+from routers import ai_agent, calculator, image_generation
 
 load_dotenv()
 
@@ -21,7 +21,7 @@ app.add_middleware(
 
 app.include_router(calculator.router, prefix="/calculator", tags=["Calculator"])
 app.include_router(ai_agent.router, prefix="/ai_agent", tags=["AI Agent"])
-# app.include_router(images.router, prefix="/images", tags=["Image Generation"])
+app.include_router(image_generation.router, prefix="/images", tags=["Image Generation"])
 
 
 @app.get("/", tags=["Health"])
--- routers/image_generation.py
+++ routers/image_generation.py
@@ -1,3 +1,15 @@
 from fastapi import APIRouter, HTTPException
+from dotenv import load_dotenv
+from generator_image.image_generator import generate_image
+from openai import AsyncOpenAI
+import os
 
+load_dotenv()
 router = APIRouter()
+client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+@router.post("/generate_image")
+async def generate_image_prompt(prompt: str):
+    result = generate_image(prompt)
+    return {"link for the image": result}
